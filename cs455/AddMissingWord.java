54d3950af47d40e1b8c3fd1984faec964f005108
Walid Budgaga <wbudgaga@gmail.com>
2020-07-21 10:31:19 -0600

    Update AddMissingWord.java

--- cs455/AddMissingWord.java
+++ cs455/AddMissingWord.java
@@ -86,8 +86,8 @@ public class AddMissingWord  extends Configured implements Tool {
 		@Override
 		protected void setup(Context context) throws IOException,InterruptedException {
 			super.setup(context);
-			String sentence = context.getConfiguration().get("SENTENCE");
-			sentenceParts = Util.parseText(sentence.toLowerCase(), ' ');
+			String sentence 		= context.getConfiguration().get("SENTENCE");
+			sentenceParts 			= Util.parseText(sentence.toLowerCase(), ' ');
 		}
 		
 		@Override
